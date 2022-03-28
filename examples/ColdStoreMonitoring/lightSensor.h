0d81fac5e915f2dd3a5d2423edf72b6380e6b17e
Sebastian Romero <s.romero@arduino.cc>
2022-03-29 00:09:24 +0200

    Lower light threshold

--- examples/ColdStoreMonitoring/lightSensor.h
+++ examples/ColdStoreMonitoring/lightSensor.h
@@ -9,12 +9,12 @@
 /**
  * @brief Get the ambient light status by reading from the light sensor.
  * On SAMD architectures it uses the internal 2V23 reference.
- * The default threshold value is 25 (out of 1000) for the light to be cosidered on.
+ * The default threshold value is 20 (out of 100) for the light to be cosidered on.
  * @param sensorPin The pin on which the light sensor is connected.
  * @param threshold The threshold for the light to be cosidered on.
  * @return True if the light is on, false otherwise
  */
-bool getAmbientLightStatus(uint8_t sensorPin, uint8_t threshold = 25) {
+bool getAmbientLightStatus(uint8_t sensorPin, uint8_t threshold = 20) {
     #if defined(ARDUINO_ARCH_SAMD)
         analogReference(AR_INTERNAL2V23);
     #endif
