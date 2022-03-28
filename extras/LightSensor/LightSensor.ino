f9b64cdf2a41f14ab4417dc462917e08ef8ff741
Sebastian Romero <s.romero@arduino.cc>
2022-03-28 22:47:35 +0200

    Improve light sensor sketch

--- extras/LightSensor/LightSensor.ino
+++ extras/LightSensor/LightSensor.ino
@@ -1,18 +1,17 @@
   
 int light_sensor = A3; 
+#define LIGHT_ON_THRESHOLD 25
  
 void setup() {
-Serial.begin(9600); //begin Serial Communication
+  Serial.begin(9600); //begin Serial Communication
 }
  
 void loop() {
   analogReference(AR_INTERNAL2V23);
   int raw_light = analogRead(light_sensor); // read the raw value from light_sensor pin (A3)
-  Serial.println(raw_light);
-  delay(100);
+  //Serial.println(raw_light);
   analogReference(AR_DEFAULT);
-  return;
-  int light = map(raw_light, 0, 1023, 0, 100); // map the value from 0, 1023 to 0, 100
+  int light = map(raw_light, 0, 940, 0, 100); // map the value from 0, 1023 to 0, 100
  
   Serial.print("Light level: "); 
   Serial.println(light); // print the light value in Serial Monitor
